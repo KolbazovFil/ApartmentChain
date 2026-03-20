9c569e047c6be057992c5f5a12ff5cc84a476d4b
KolbazovFil <mr.kolbazov@yandex.ru>
2026-03-20 21:06:06 +0300

    обновление скрипта, добавление скролвьюера в профиль

--- Model.Context.cs
+++ Model.Context.cs
@@ -21,13 +21,11 @@ namespace ApartmentChain
         }
 
         private static Entities _context;
-
         public static Entities GetContext()
         {
             if (_context == null) _context = new Entities();
             return _context;
         }
-    
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             throw new UnintentionalCodeFirstException();
